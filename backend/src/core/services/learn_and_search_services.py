74c7c117b09bb0aedce4611e07445c1117a38331
Shlomi Ganon <Shlomi1395@Gmail.com>
2026-04-14 12:34:02 +0300

    declere steps

--- backend/src/core/services/learn_and_search_services.py
+++ backend/src/core/services/learn_and_search_services.py
@@ -1,5 +1,14 @@
 from io import IO
 from src.core.io.dataset_reader import read_posts_from_csv
+
+#LEARN : (csv_file) -> add a faces to the database and ivf 
+#SEARCH : (media_file) -> search for a face in the database and return the most similar faces
+#* SEARCH_API : (first_name, last_name , media_file) -> search for a face in the database and return the most similar faces
+
+#we need to redesign the all backend 
+
+#link the frontend to the backend (to use the api)
+
 def learn_service(csv_file: IO[str]) -> None:
     posts = read_posts_from_csv(csv_file)
     #need to ingest_post for each post
